50517ab15e7656f2644576213bc35748c5741b70
naram Kim <sylvester127@naver.com>
2019-11-09 19:39:05 +0900

    fear: Add RankingService interface and implement codes

--- mainProject/src/com/stockmarket/www/service/RankingService.java
+++ mainProject/src/com/stockmarket/www/service/RankingService.java
@@ -2,8 +2,10 @@ package com.stockmarket.www.service;
 
 import java.util.List;
 
-import com.stockmarket.www.model.Member;
+import com.stockmarket.www.ett.Member;
 
 public interface RankingService {
+	List<Member> getMemberList();
 	
+	Member getMember(int id);
 }
