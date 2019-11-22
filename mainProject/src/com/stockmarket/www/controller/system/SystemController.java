c488a28ce9153f275a1656dddcd0c0a6441eec86
bluewow <dogseen@gmail.com>
2019-11-22 18:15:05 +0900

    jsoup build path 추가 및 현재주가 크롤링(임시)

--- mainProject/src/com/stockmarket/www/controller/system/SystemController.java
+++ mainProject/src/com/stockmarket/www/controller/system/SystemController.java
@@ -9,6 +9,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+
+
 // TODO
 // 5분 간격으로 주식 가격 크롤링
 // 장종료후 주식데이터 갱신
@@ -34,6 +40,8 @@ public class SystemController extends HttpServlet {
 				
 			} catch (InterruptedException e) {
 				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}	
 		});
 		
@@ -41,22 +49,43 @@ public class SystemController extends HttpServlet {
 		request.getRequestDispatcher("main.jsp").forward(request, response);
 	}
 
-	private void systemThread() throws InterruptedException {
+	private void systemThread() throws InterruptedException, IOException {
 
 		SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String format_time1 = date.format(System.currentTimeMillis());
 //		System.out.println(format_time1);
 		
-		//주식정보 크롤링
+		//주식가격 refresh by 크롤링
+		refreshStockPrice();
 		
 		//6시 장종료후 주식데이터 갱신 
 		
-		
+
 		Thread.sleep(1000 * 60 * 5);
+	}
+
+	private void refreshStockPrice() throws IOException {
+		Document doc = null;
+		String url = "https://finance.naver.com/item/main.nhn?code=140410";
+		
+		
+		doc = Jsoup.connect(url).ignoreContentType(true)
+								.timeout(5000)
+								.get();
+		
+		//현재가 가져오기 (임시)
+		Element element = doc.selectFirst("p.no_today span");
+//		System.out.println(element.text());
+		
+		
+		
+		
 	}
 	
 //	for TEST
-//	public static void main(String[] args) {
-//		
-//	}
+	public static void main(String[] args) throws IOException {
+		SystemController sys = new SystemController();
+		
+		sys.refreshStockPrice();
+	}
 }
