69efd3a86eed1fe8dc390390bb69e1f1b6500fdd
bluewow <dogseen@gmail.com>
2019-12-03 01:36:27 +0900

    popup js refactoring

--- mainProject/WebContent/js/popup/popup.js
+++ mainProject/WebContent/js/popup/popup.js
@@ -1,127 +1,143 @@
-//로그인 팝업 
-window.addEventListener("load", loginFunc);
-//로그인/로그아웃/프로필 버튼 
-window.addEventListener("load", buttonFunc);
-//팝업 hidden
-window.addEventListener("load", hiddenFunc);
-//회원가입 팝업
-window.addEventListener("load", signUpFunc);
-//프로필설정 팝업
-window.addEventListener("load", profileFunc);
+window.addEventListener("load", function(){
+	
+	loginFunc();	//로그인 팝업
+	buttonFunc();	//로그인/로그아웃/프로필 버튼 
+	hiddenFunc();	//팝업 hidden
+	signUpFunc();	//회원가입 팝업
+	profileFunc();	//프로필설정 팝업
+	
+	////////////////////////////
+	//로그인 팝업 
+	////////////////////////////
+	function loginFunc() {
+	    var loginPopup = document.querySelector(".pop-up");
+	    var signupOn = document.querySelector(".sign-up-pop-up");
+	    
+	    loginPopup.onclick = function(e) {
+	        if(e.target.nodeName != "INPUT")
+	            return;
 
-//로그인 팝업 
-function loginFunc() {
-    var loginPopup = document.querySelector(".pop-up");
-    var signupOn = document.querySelector(".sign-up-pop-up");
+	        //prevent Event Bubble
+	        e.preventDefault();
 
-    loginPopup.onclick = function(e) {
-        if(e.target.nodeName != "INPUT")
-            return;
+	        if(e.target.value =="로그인") {
+	            //TODO 이메일 양식 비밀번호 체크
+	        	var form = loginPopup.querySelector(".login-box");
+//	        	document.querySelector("#login").submit();
 
-        //prevent Event Bubble
-        e.preventDefault();
+	        	form.submit();
+	            
+	        }
+	        else if(e.target.value == "무료회원가입") {
+	            loginPopup.style.visibility = "hidden";
+	            signupOn.style.visibility = "visible";
 
-        if(e.target.value =="로그인") {
-            //TODO 이메일 양식 비밀번호 체크
-        	var form = loginPopup.querySelector(".login-box");
-//        	document.querySelector("#login").submit();
+	        } else if(e.target.value == "이메일/비밀번호 찾기") {
+	            alert("이메일");
+	        } 
+	    }
+	}
+	
+	////////////////////////////
+	//로그인/로그아웃/프로필 버튼 관련 함수
+	////////////////////////////
+	function buttonFunc() {
+	    var popupOn = document.querySelector(".personal");
+	    var wrapper = document.querySelector(".pop-up-wrapper");
 
-        	form.submit();
-            
-        }
-        else if(e.target.value == "무료회원가입") {
-            loginPopup.style.visibility = "hidden";
-            signupOn.style.visibility = "visible";
+	    popupOn.onclick = function(e) {
+	        //로그인, 로그아웃, UserName filter
+	        if(e.target.nodeName != "INPUT")
+	            return;
 
-        } else if(e.target.value == "이메일/비밀번호 찾기") {
-            alert("이메일");
-        } 
-    }
-}
+	        //prevent Event Bubble
+	        e.preventDefault();
 
-//로그인/로그아웃/프로필 버튼 관련 함수
-function buttonFunc() {
-    var popupOn = document.querySelector(".personal");
-    var wrapper = document.querySelector(".pop-up-wrapper");
+	        if(e.target.value == "로그인") {
+	            //로그인 창 및 background color visible
+	            var loginPopup = document.querySelector(".pop-up");
+	            wrapper.style.visibility = "visible";
+	            loginPopup.style.visibility = "visible";
 
-    popupOn.onclick = function(e) {
-        //로그인, 로그아웃, UserName filter
-        if(e.target.nodeName != "INPUT")
-            return;
+	        } else if(e.target.value == "로그아웃") {
+	            //로그아웃시 session 만료
+	            var request = new XMLHttpRequest();
+	            request.open("GET", "/login?loginStatus=logout");
+	            request.onload = function() {
+	                location.reload(true);
+	            }
+	            request.send();
 
-        //prevent Event Bubble
-        e.preventDefault();
+	        } else {
+	            //프로필
+	             var profilePopup = document.querySelector(".profile-pop-up");
+	             wrapper.style.visibility = "visible";
+	             profilePopup.style.visibility = "visible";
+	        }
+	    }
+	}
+	
+	
+	////////////////////////////
+	//팝업 hidden 처리
+	////////////////////////////
+	function hiddenFunc() {
+	    window.onclick = function(e) {
+	        var wrapper = document.querySelector(".pop-up-wrapper");
+	        var loginPopup = document.querySelector(".pop-up");
+	        var signupPopup = document.querySelector(".sign-up-pop-up");
+	        var profilePopup = document.querySelector(".profile-pop-up");
 
-        if(e.target.value == "로그인") {
-            //로그인 창 및 background color visible
-            var loginPopup = document.querySelector(".pop-up");
-            wrapper.style.visibility = "visible";
-            loginPopup.style.visibility = "visible";
+	        if (e.target == wrapper) {
+	            wrapper.style.visibility = "hidden";
+	            loginPopup.style.visibility = "hidden";
+	            signupPopup.style.visibility = "hidden";
+	            profilePopup.style.visibility = "hidden";
+	        }
+	    }
+	}
+	
+	////////////////////////////
+	//회원가입 팝업
+	////////////////////////////
+	function signUpFunc() {
+	    var signupPopup = document.querySelector(".sign-up-pop-up");
 
-        } else if(e.target.value == "로그아웃") {
-            //로그아웃시 session 만료
-            var request = new XMLHttpRequest();
-            request.open("GET", "/login?loginStatus=logout");
-            request.send();
-            request.onload = function() {
-                location.reload(true);
-            }
+	    signupPopup.onclick = function(e) {
+	        if(e.target.nodeName != "INPUT")
+	            return;
 
-        } else {
-            //프로필
-             var profilePopup = document.querySelector(".profile-pop-up");
-             wrapper.style.visibility = "visible";
-             profilePopup.style.visibility = "visible";
-        }
-    }
-}
+	        //prevent Event Bubble
+	        e.preventDefault();
 
-//팝업 hidden 처리
-function hiddenFunc() {
-    window.onclick = function(e) {
-        var wrapper = document.querySelector(".pop-up-wrapper");
-        var loginPopup = document.querySelector(".pop-up");
-        var signupPopup = document.querySelector(".sign-up-pop-up");
-        var profilePopup = document.querySelector(".profile-pop-up");
+	        if(e.target.nodeName == "userEmail") {
+	            alert("TEST");
+	        } else if(e.target.nodeName == "nickName") {
 
-        if (e.target == wrapper) {
-            wrapper.style.visibility = "hidden";
-            loginPopup.style.visibility = "hidden";
-            signupPopup.style.visibility = "hidden";
-            profilePopup.style.visibility = "hidden";
-        }
-    }
-}
+	        } else if(e.target.nodeName == "pwd") {
+	        
+	        } else if(e.target.nodeName == "checkPwd") {
+	        
+	        } else if(e.target.value == "회원가입") {
+//	            var form = signupPopup.querySelector(".login-box");
+	        	document.querySelector("#signup").submit();
 
-//회원가입 팝업
-function signUpFunc() {
-    var signupPopup = document.querySelector(".sign-up-pop-up");
+//	        	form.submit();
+	        }
+	    }
+	}
 
-    signupPopup.onclick = function(e) {
-        if(e.target.nodeName != "INPUT")
-            return;
+	////////////////////////////
+	//프로필설정 팝업
+	///////////////////////////
+	function profileFunc() {
 
-        //prevent Event Bubble
-        e.preventDefault();
+	}
+	
+});
 
-        if(e.target.nodeName == "userEmail") {
-            alert("TEST");
-        } else if(e.target.nodeName == "nickName") {
 
-        } else if(e.target.nodeName == "pwd") {
-        
-        } else if(e.target.nodeName == "checkPwd") {
-        
-        } else if(e.target.value == "회원가입") {
-//            var form = signupPopup.querySelector(".login-box");
-        	document.querySelector("#signup").submit();
 
-//        	form.submit();
-        }
-    }
-}
 
-//프로필설정 팝업
-function profileFunc() {
 
-}
+
