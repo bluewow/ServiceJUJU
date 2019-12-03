8e6419cc70ee4b2473a25fb29b5df3ec2d1bea27
bluewow <dogseen@gmail.com>
2019-12-03 13:11:42 +0900

    stockDao

--- mainProject/src/com/stockmarket/www/dao/jdbc/JdbcStockDao.java
+++ mainProject/src/com/stockmarket/www/dao/jdbc/JdbcStockDao.java
@@ -3,6 +3,8 @@ package com.stockmarket.www.dao.jdbc;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
+
 import com.stockmarket.www.dao.StockDao;
 
 public class JdbcStockDao implements StockDao {
@@ -10,30 +12,25 @@ public class JdbcStockDao implements StockDao {
 	@Override
 	public String getStockName(String codeNum) {
 		String sql = "SELECT * FROM STOCK WHERE CODENUM=?";
+		JdbcDaoContext daoContext = new JdbcDaoContext();
 		PreparedStatement pst = null;
 		ResultSet rs = null;
 
 		try {
-			pst = JdbcDaoContext.getPreparedStatement(sql);
+			pst = daoContext.getPreparedStatement(sql);
 			pst.setString(1, codeNum);
 			rs = pst.executeQuery();
 
 			if (rs.next()) {
 				return rs.getString("NAME");
 			}
+			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			try {
-				if (rs != null)
-					rs.close();
-				if (pst != null)
-					pst.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			daoContext.close(rs, pst);
 		}
 		return null;
 	}
@@ -41,11 +38,12 @@ public class JdbcStockDao implements StockDao {
 	@Override
 	public String getStockCodeNum(String name) {
 		String sql = "SELECT * FROM STOCK WHERE name=?";
+		JdbcDaoContext daoContext = new JdbcDaoContext();
 		PreparedStatement pst = null;
 		ResultSet rs = null;
-
+		
 		try {
-			pst = JdbcDaoContext.getPreparedStatement(sql);
+			pst = daoContext.getPreparedStatement(sql);
 			pst.setString(1, name);
 			rs = pst.executeQuery();
 
@@ -58,14 +56,7 @@ public class JdbcStockDao implements StockDao {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			try {
-				if (rs != null)
-					rs.close();
-				if (pst != null)
-					pst.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			daoContext.close(rs, pst);
 		}
 		return null;
 	}
@@ -76,5 +67,17 @@ public class JdbcStockDao implements StockDao {
 
 		System.out.println(stockDao.getStockName("005380"));
 		System.out.println(stockDao.getStockCodeNum("현대차"));
+		
+		while(true) {
+			System.out.println("Input ");
+			Scanner sc = new Scanner(System.in);
+			int input = sc.nextInt(); 
+			
+			for(int i = 0; i < input; i++) {
+				stockDao.getStockName("005380");
+				stockDao.getStockCodeNum("현대차");
+			}
+				
+		}
 	}
 }
