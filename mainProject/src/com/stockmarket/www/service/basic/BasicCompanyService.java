3248e81001f5fb19595f4a51fcdb71e56153ffe7
jungho <jungpark007@naver.com>
2019-12-11 14:33:33 +0900

    에러 메세지 제거

--- mainProject/src/com/stockmarket/www/service/basic/BasicCompanyService.java
+++ mainProject/src/com/stockmarket/www/service/basic/BasicCompanyService.java
@@ -43,127 +43,84 @@ public class BasicCompanyService implements CompanyService {
 
 	}
 
-	@Override
-	public List<Company> getCompanyListFromNaverByThema(String companyName) {
+	// 네이버 종목은 필요가 없기 때문에 주석처리
+	/*
+	 * @Override public List<Company> getCompanyListFromNaverByThema(String
+	 * companyName) {
+	 * 
+	 * 
+	 * 네이버 업종별 크롤링을 위한
+	 * 코드==========================================================================
+	 * 
+	 * 
+	 * String urlSector = "https://finance.naver.com/sise/theme.nhn";
+	 * 
+	 * Document docurlSector = null;
+	 * 
+	 * try { docurlSector = Jsoup.connect(urlSector).get(); } catch (IOException e)
+	 * { e.printStackTrace(); }
+	 * 
+	 * Elements elementSector = docurlSector.select(".col_type1"); //
+	 * System.out.println(elementSector); //
+	 * 
+	 * Elements SectorAtag = docurlSector.select(".col_type1 a"); //
+	 * System.out.println(SectorAtag); // a 태그만 출력
+	 * 
+	 * // Iterator<Element> ie1Atag = elementSector.select("a").iterator();
+	 * Iterator<Element> sectorIterator = SectorAtag.select("a").iterator();
+	 * 
+	 * // 참고: https://finance.naver.com/ + 주소값 int cnt = 0;
+	 * 
+	 * ArrayList<String> sectorNames = new ArrayList<String>(); ArrayList<String>
+	 * sectorHomepageURLs = new ArrayList<String>();
+	 * 
+	 * // 테마 목록에서 기업정보 얻어내기
+	 * 
+	 * int sectorIndex = 0;
+	 * 
+	 * for (int i = 0; i < sectorNames.size(); i++) if
+	 * (sectorNames.get(i).equals(companyName)) { sectorIndex = i; break; }
+	 * 
+	 * // company 목록 가져오기
+	 * 
+	 * if (sectorIndex != 0) { List<Company> companyArrayList = new
+	 * ArrayList<Company>();
+	 * 
+	 * String sectorUrl = "https://finance.naver.com/" +
+	 * sectorHomepageURLs.get(sectorIndex); Document sectorUrlDoc = null;
+	 * 
+	 * try { sectorUrlDoc = Jsoup.connect(sectorUrl).get(); } catch (IOException e)
+	 * {
+	 * 
+	 * e.printStackTrace(); }
+	 * 
+	 * Elements stockElement = sectorUrlDoc.select(".type_5"); Iterator<Element>
+	 * stockElement_ = stockElement.select("td a").iterator();
+	 * 
+	 * while (stockElement_.hasNext()) {
+	 * 
+	 * companyArrayList.add(csvStockDataDao.searchCompany(stockElement_.next().text(
+	 * )));
+	 * 
+	 * // 1. 가져온 기업이름으로 'StockDataDao'에서 search 한다.
+	 * 
+	 * // 2. 그 결과로 Company
+	 * 
+	 * }
+	 * 
+	 * HashSet<Company> companyArrayList2 = new HashSet<Company>(companyArrayList);
+	 * // HashSet에 arr데이터 삽입 ArrayList<Company> companyArrayList3 = new
+	 * ArrayList<Company>(companyArrayList2); // 중복이 제거된 HashSet을 다시 // ArrayList에
+	 * 삽입
+	 * 
+	 * return companyArrayList3; }
+	 * 
+	 * return null;
+	 * 
+	 * }
+	 */
 
-		/*
-		 * 네이버 업종별 크롤링을 위한
-		 * 코드==========================================================================
-		 */
-
-		String urlSector = "https://finance.naver.com/sise/theme.nhn";
-
-		Document docurlSector = null;
-
-		try {
-			docurlSector = Jsoup.connect(urlSector).get();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		Elements elementSector = docurlSector.select(".col_type1");
-		// System.out.println(elementSector); //
-
-		Elements SectorAtag = docurlSector.select(".col_type1 a");
-		// System.out.println(SectorAtag); // a 태그만 출력
-
-		// Iterator<Element> ie1Atag = elementSector.select("a").iterator();
-		Iterator<Element> sectorIterator = SectorAtag.select("a").iterator();
-
-		// 참고: https://finance.naver.com/ + 주소값
-		int cnt = 0;
-
-		ArrayList<String> sectorNames = new ArrayList<String>();
-		ArrayList<String> sectorHomepageURLs = new ArrayList<String>();
-
-		// 테마 목록에서 기업정보 얻어내기
-
-		int sectorIndex = 0;
-
-		for (int i = 0; i < sectorNames.size(); i++)
-			if (sectorNames.get(i).equals(companyName)) {
-				sectorIndex = i;
-				break;
-			}
-
-		// company 목록 가져오기
-
-		if (sectorIndex != 0) {
-			List<Company> companyArrayList = new ArrayList<Company>();
-
-			String sectorUrl = "https://finance.naver.com/" + sectorHomepageURLs.get(sectorIndex);
-			Document sectorUrlDoc = null;
-
-			try {
-				sectorUrlDoc = Jsoup.connect(sectorUrl).get();
-			} catch (IOException e) {
-
-				e.printStackTrace();
-			}
-
-			Elements stockElement = sectorUrlDoc.select(".type_5");
-			Iterator<Element> stockElement_ = stockElement.select("td a").iterator();
-
-			while (stockElement_.hasNext()) {
-
-				companyArrayList.add(csvStockDataDao.searchCompany(stockElement_.next().text()));
-
-				// 1. 가져온 기업이름으로 'StockDataDao'에서 search 한다.
-
-				// 2. 그 결과로 Company
-
-			}
-
-			HashSet<Company> companyArrayList2 = new HashSet<Company>(companyArrayList); // HashSet에 arr데이터 삽입
-			ArrayList<Company> companyArrayList3 = new ArrayList<Company>(companyArrayList2); // 중복이 제거된 HashSet을 다시
-																								// ArrayList에 삽입
-
-			return companyArrayList3;
-		}
-		/*
-		 * List<Company> companyArrayList = new ArrayList<Company>();
-		 * 
-		 * 
-		 * String sectorUrl = "https://finance.naver.com/"
-		 * +sectorHomepageURLs.get(sectorIndex); Document sectorUrlDoc = null;
-		 * 
-		 * try { sectorUrlDoc = Jsoup.connect(sectorUrl).get(); } catch (IOException e)
-		 * {
-		 * 
-		 * e.printStackTrace(); }
-		 * 
-		 * 
-		 * 
-		 * 
-		 * Elements stockElement = sectorUrlDoc.select(".type_5"); Iterator<Element>
-		 * stockElement_ = stockElement.select("td a").iterator();
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * while (stockElement_.hasNext()) {
-		 * 
-		 * companyArrayList.add(csvStockDataDao.searchCompany(stockElement_.next().text(
-		 * )));
-		 * 
-		 * //1. 가져온 기업이름으로 'StockDataDao'에서 search 한다.
-		 * 
-		 * //2. 그 결과로 Company
-		 * 
-		 * 
-		 * }
-		 * 
-		 * HashSet<Company> companyArrayList2 = new HashSet<Company>(companyArrayList);
-		 * // HashSet에 arr데이터 삽입 ArrayList<Company> companyArrayList3 = new
-		 * ArrayList<Company>(companyArrayList2); // 중복이 제거된 HashSet을 다시 ArrayList에 삽입
-		 * 
-		 * return companyArrayList3;
-		 */
-		return null;
-
-	}
-
+	
 	/* 네이버 업종 크롤링 */
 	@Override
 	public void stockIndustryCrawling() {
@@ -191,92 +148,46 @@ public class BasicCompanyService implements CompanyService {
 		while (IndustryName.hasNext()) {
 			upjonName.add(IndustryName.next().text());
 		}
-		//System.out.println(upjonName);
+		// System.out.println(upjonName);
 		while (IndustryAtag.hasNext()) {
 			upjongAtag.add(IndustryAtag.next().attr("href"));
 		}
-		//System.out.println(upjongAtag); ==== 여기까지 출력 됨....
+		// System.out.println(upjongAtag); ==== 여기까지 출력 됨....
 
 		// 1. 업종명과 링크를 얻는다.
 		// 2. 업종명에 해당하는 링크를 타고 들어가서 상세 종목명을 얻는다.
 		// 일단 여기까지..
-		
-		ArrayList <String> detailIndustryList = new ArrayList<>();
+
+		ArrayList<String> detailIndustryList = new ArrayList<>();
 		for (int i = 0; i < upjonName.size(); i++) {
 			String upjongDetailUrl = "https://finance.naver.com" + upjongAtag.get(i);
 			Document upjongDetail = null;
-			
+
 			detailIndustryList.add(upjonName.get(i));
 
-			
 			try {
 				upjongDetail = Jsoup.connect(upjongDetailUrl).get();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
-			Elements detailIndustrytable = upjongDetail.select("tbody a");
-			Iterator<Element> detailIndustryName = detailIndustrytable.select("a").iterator();
-			
-			
-			
+
+			Elements detailIndustryTable = upjongDetail.select("tbody a");
+			Iterator<Element> detailIndustryName = detailIndustryTable.select("a").iterator();
+
 			while (detailIndustryName.hasNext()) {
 				detailIndustryList.add(detailIndustryName.next().text());
 			}
-			
+
 		}
-		
+
 		System.out.println(detailIndustryList);
-//		ArrayList <String> detailIndustryList = new ArrayList<>();
-//		String upjongDetailUrl = "https://finance.naver.com" + upjongAtag.get(0);
-//		Document upjongDetail = null;
-//		
-//		detailIndustryList.add(upjonName.get(0));
-//
-//		
-//		try {
-//			upjongDetail = Jsoup.connect(upjongDetailUrl).get();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		
-//		Elements detailIndustrytable = upjongDetail.select("tbody a");
-//		Iterator<Element> detailIndustryName = detailIndustrytable.select("a").iterator();
-//		
-//		
-//		
-//		while (detailIndustryName.hasNext()) {
-//			detailIndustryList.add(detailIndustryName.next().text());
-//			System.out.println(detailIndustryList);
-//		}
-		
-		
-		
-//		for (int i = 0; i < upjongAtag.size(); i++) {
-//			String upjongDetailUrl = "https://finance.naver.com" + upjongAtag.get(i);
-//			Document upjongDoc = null;
-//
-//			//System.out.println("upjongDetailUrl" + upjongDetailUrl);
-//			try {
-//				upjongDoc = Jsoup.connect(upjongDetailUrl).get();
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//
-//			Elements detailIndustrytable = upjongDoc.select("tbody a");
-//			//System.out.println(detailIndustrytable);
-//			Iterator<Element> detailIndustryName = detailIndustrytable.select("a").iterator();
-//			
-//			ArrayList<String> detailIndustryNameList = new ArrayList<>();
-//			//ArrayList<String> finalDetailIndustryNameList = new ArrayList<>();
-//			
-////			while (detailIndustryName.hasNext()) {
-////				Map<String, Object> finalDetailIndustryNameList = new HashMap();
-////				//detailIndustryNameList.add(detailIndustryName.next().text());
-////				finalDetailIndustryNameList.put(upjonName.get(i), detailIndustryName.next().text());
-////			}
-//			
-//		}
+
+	}
+
+	@Override
+	public List<Company> getCompanyListFromNaverByThema(String companyName) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
