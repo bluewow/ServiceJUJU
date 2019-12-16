105524dcc0e29ba07fd691d158715310c646d3c3
terry894 <terry894@naver.com>
2019-12-16 19:57:35 +0900

    hodinglist 1차 초안

--- mainProject/WebContent/js/managestocks/holding_list.js
+++ mainProject/WebContent/js/managestocks/holding_list.js
@@ -3,6 +3,7 @@ window.addEventListener("load",function(){
    var section = this.document.querySelector(".manageStocks");
    var updateButton = section.querySelector(".updateButton");
    var preArea = section.querySelector(".prearea");
+   var backArea = section.querySelector(".backarea");
    var tbody = section.querySelector("table tbody");
 
 	function addComma(num) {
@@ -20,19 +21,16 @@ window.addEventListener("load",function(){
 
     //서블릿의 실행이 완료되었을때 실행 
     request.onload = function(){
-
 //        var cardFooter = section.querySelector(".card-footer");
-//        console.log(cardFooter.firstChild);
-        
+//        console.log(cardFooter.firstChild);    
         var list = JSON.parse(request.responseText);
         
-        var allIncomePercent =0
+        var won = "원";
+        var allIncomePercent =0;
         var allIncome = 0;
         var allSum = 0;
         tbody.innerHTML = "";
         
-
-
         for(var i=0; i<list.length; i++){
             
          var template = section.querySelector(".template");
@@ -43,7 +41,8 @@ window.addEventListener("load",function(){
          var incomePercent = Math.ceil((list[i].income/list[i].sum)*100);
          allIncome += list[i].income;
          allSum += list[i].sum;
-         allIncomePercent = Math.ceil((allIncome/allSum)*100);
+         allIncomePercent += incomePercent;
+         
          list[i].income = addComma(list[i].income);
          list[i].sum = addComma(list[i].sum);
 
@@ -77,12 +76,6 @@ window.addEventListener("load",function(){
               tds[j].style.display = "none";
             }
          }
-        console.log(allIncomePercent+",allSum:"+allSum+",allIncome:"+allIncome); 
-        
-         if(allIncomePercent>0)
-         {
-        	  preArea.firstElementChild.nextSibling.innerText = allIncomePercent;
-         }
 
          // if(IncomePercent>0)
          // {
@@ -99,8 +92,38 @@ window.addEventListener("load",function(){
         //   var tds = cloneTr.querySelectorAll("td");
         //   tds[0].innertext = list[i].stockName;
         tbody.append(cloneTr);
+        
         }
- 
+        
+       allIncomePercent = allIncomePercent/list.length
+       console.log(allIncomePercent+",allSum:"+allSum+",allIncome:"+allIncome); 
+       var Valuation = allSum - allIncome; 
+
+       if(allIncomePercent>0){
+        preArea.firstElementChild.nextElementSibling.style.color ="red";
+        preArea.firstElementChild.nextElementSibling.innerText = allIncomePercent + "%";
+        preArea.lastElementChild.style.color ="red";
+        preArea.lastElementChild.innerText = addComma(allIncome) + won;
+        backArea.lastElementChild.style.color = "red";
+        backArea.lastElementChild.innerText = addComma(Valuation) + won;
+        
+       }
+       else if (allIncomePercent<0){
+        preArea.firstElementChild.nextElementSibling.style.color ="blue";
+        preArea.firstElementChild.nextElementSibling.innerText = allIncomePercent + "%";
+        preArea.lastElementChild.style.color ="blue";
+        preArea.lastElementChild.innerText = addComma(allIncome) + won; 
+        backArea.lastElementChild.style.color = "blue";
+        backArea.lastElementChild.innerText = addComma(Valuation) + won;
+       }
+       else{
+        preArea.firstElementChild.nextElementSibling.innerText = "0%";
+        preArea.lastElementChild.innerText = 0 + won;
+        backArea.lastElementChild.innerText = 0 + won;
+       }
+       
+       backArea.firstElementChild.nextElementSibling.innerText = addComma(allSum) + won;
+       
     };
     request.send();
    }
