cf29f31451f38eb44711e0093a9a358acb248fd9
bluewow <dogseen@gmail.com>
2019-12-16 13:06:03 +0900

    capture content 주석추가

--- mainProject/WebContent/js/capture/capture.js
+++ mainProject/WebContent/js/capture/capture.js
@@ -1,5 +1,7 @@
 window.addEventListener("message", function(e) {
 	if(e.data.capture)
-		console.log(e.data.capture); //json format
+		console.log(e.data.capture); 
+		//json format
+		//CaptureMemo PER/PBR/ROE/debtRatio/marketCap/codeNum/memberID
 		
 });
