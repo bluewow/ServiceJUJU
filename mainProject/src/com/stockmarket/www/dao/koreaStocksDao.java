5a2a34785b47e2d0570b07657a476a3d22492d94
jungho <jungpark007@naver.com>
2019-12-26 09:29:11 +0900

    테스트

--- mainProject/src/com/stockmarket/www/dao/koreaStocksDao.java
+++ mainProject/src/com/stockmarket/www/dao/koreaStocksDao.java
@@ -3,7 +3,7 @@ package com.stockmarket.www.dao;
 import java.util.List;
 
 import com.stockmarket.www.entity.koreaStocks;
-
+//koreaStocks = 코스피+ 코스닥
 public interface koreaStocksDao {
 	List<koreaStocks> getList();
 	
