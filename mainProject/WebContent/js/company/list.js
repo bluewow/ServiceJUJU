7b66e4eaa92f116d8cd25f5e7475801024d6ddc4
bluewow <dogseen@gmail.com>
2019-12-27 00:28:36 +0900

    검색결과가 1개이상일 경우에도 codeNum 이 전달되도록 수정

--- mainProject/WebContent/js/company/list.js
+++ mainProject/WebContent/js/company/list.js
@@ -1,37 +1,33 @@
 window.addEventListener("load", function(){
 	//codeNum 전달이벤트
-//	sendEvent();
-//	
-//	function sendEvent() {
-//	    var companyName = document.querySelector("#companyName");
-//	    
-//	    if(companyName == null) 
-//	    	return;
-//	    
-//	    companyName.onclick = function(e){
-//	       var codenum = companyName.dataset.codenum;
-//	       
-//	       var stockBoardWindow = parent.document.querySelector("#stock-board-window");
-//	       var analysisWindow = parent.document.querySelector("#analysis-window");
-//	       var tradeWindow = parent.document.querySelector("#trade-window");
-//	       
-//	       stockBoardWindow.contentWindow.postMessage(
-//	    		   codenum, "http://localhost:8080/card/board/stock_board.jsp");
-//	       analysisWindow.contentWindow.postMessage(
-//	    		   codenum, "http://localhost:8080/card/trade/analysis.jsp");
-//	       tradeWindow.contentWindow.postMessage(
-//	    		   codenum, "http://localhost:8080/card/trade/trading.jsp");
-//	    }
-//	}
-//	
+	sendEvent();
+	
+	function sendEvent() {
+	    var companyName = document.querySelectorAll(".companyName");
+	    if(companyName == null) 
+	    	return;
+	    
+	    for(i = 0; i < companyName.length; i++) {
+		    companyName[i].onclick = function(e){
+		       var codenum = e.target.dataset.codenum;
+		       var stockBoardWindow = parent.document.querySelector("#stock-board-window");
+		       var analysisWindow = parent.document.querySelector("#analysis-window");
+		       var tradeWindow = parent.document.querySelector("#trade-window");
+		       
+		       stockBoardWindow.contentWindow.postMessage(
+		    		   codenum, "http://localhost:8080/card/board/stock_board.jsp");
+		       analysisWindow.contentWindow.postMessage(
+		    		   codenum, "http://localhost:8080/card/trade/analysis.jsp");
+		       tradeWindow.contentWindow.postMessage(
+		    		   codenum, "http://localhost:8080/card/trade/trading.jsp");
+		    }
+	    }
+	}
+	
 //	var recommendKeyword = document.querySelector("#ajaxTest");
 //	recommendKeyword.onclick = function(e){
 //		alert("연결 확인");
 //		var ajax = new XMLHttpRequest();
 //		ajax.open("GET", "../../card/trade/list?companyName=${r}");
 //	}
-
-	
-
-
 });
