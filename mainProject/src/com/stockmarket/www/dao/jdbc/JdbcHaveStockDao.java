9973e347267ef9491b9e68a5c0c3ec131840c79a
egg93 <930608a@daum.net>
2019-12-26 17:58:12 +0900

    // 자산 분포도 테스트 주석 처리

--- mainProject/src/com/stockmarket/www/dao/jdbc/JdbcHaveStockDao.java
+++ mainProject/src/com/stockmarket/www/dao/jdbc/JdbcHaveStockDao.java
@@ -122,14 +122,14 @@ public class JdbcHaveStockDao implements HaveStockDao {
 		JdbcDaoContext context = new JdbcDaoContext();
 		
 		List<CurStock> list = new ArrayList<>();
-		list.add(new CurStock("035420", "13,000", "상승", "3,000", "+", "2.5"));
-		list.add(new CurStock("000660", "15,000", "하강", "3,000", "-", "3.4"));
-		list.add(new CurStock("020560", "16,000", "보합", "3,000", "0.0", "1.5"));
-		list.add(new CurStock("005930", "12,000", "상승", "3,000", "+", "1.6"));
-		list.add(new CurStock("005380", "11,000", "상승", "3,000", "+", "8.9"));
+		list.add(new CurStock("035420", "3,000", "상승", "3,000", "+", "2.5"));
+		list.add(new CurStock("000660", "5,000", "하강", "3,000", "-", "3.4"));
+		list.add(new CurStock("020560", "6,000", "보합", "3,000", "0.0", "1.5"));
+		list.add(new CurStock("005930", "2,000", "상승", "3,000", "+", "1.6"));
+		list.add(new CurStock("005380", "1,000", "상승", "3,000", "+", "8.9"));
 		list.add(new CurStock("095660", "10,500", "상승", "3,000", "+", "10.2"));
-		list.add(new CurStock("000880", "13,500", "하강", "3,000", "-", "14.2"));
-		list.add(new CurStock("215600", "17,000", "하강", "3,000", "-", "10"));
+		list.add(new CurStock("000880", "3,500", "하강", "3,000", "-", "14.2"));
+		list.add(new CurStock("215600", "7,000", "하강", "3,000", "-", "10"));
 
 		try {	
 
