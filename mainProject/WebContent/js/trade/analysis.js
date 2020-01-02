10506b767464aeae4341b241c88a7842847076f6
bluewow <dogseen@gmail.com>
2020-01-02 09:35:44 +0900

    billboard chart init format 적용

--- mainProject/WebContent/js/trade/analysis.js
+++ mainProject/WebContent/js/trade/analysis.js
@@ -1,30 +1,123 @@
-window.addEventListener("message", function(e) {
-	if(e.data && (e.data.length == 6)) //codeNum
-		console.log("analysis : " + e.data); 
-});
-
-window.addEventListener("load", function(){ 
-	//캡쳐하기
-	captureButton();
-	
-	function captureButton() {
-		var button = document.querySelector("#capture");
-		
-		if(button == null)
-			return;
-		
-		button.onclick = function(e) {
-			var ajax = new XMLHttpRequest();
-			ajax.open("GET", "../../card/trade/analysis?capture=on");
-			ajax.onload = function() {
-				//data send to capture Card
-				var frame = parent.document.querySelector("#capture-window");
-				frame.contentWindow.postMessage(
-						{capture: ajax.responseText }, 
-						"http://localhost:8080/card/capturememo/captureMemo.jsp");
-			}
-			ajax.send();
+window.addEventListener("load", function() {
+	var chartA = bb.generate({
+		bindto : "#pieChartA",
+		size : {
+			height : 150,
+			width : 120
+		},
+		data : {
+			columns : [ [ "data1", 30 ], [ "data2", 120 ] ],
+			type : "donut",
+		},
+		donut : {
+			title : "종목 동향",
+			label : {
+				show : false,
+			},
+		},
+		legend : {
+			show : false
 		}
-	}
-	
+	});
+
+	var chartB = bb.generate({
+		bindto : "#pieChartB",
+		size : {
+			height : 150,
+			width : 120
+		},
+		data : {
+			columns : [ [ "data1", 30 ], [ "data2", 120 ] ],
+			type : "donut",
+		},
+		donut : {
+			title : "수급",
+			label : {
+				show : false,
+			},
+		},
+		legend : {
+			show : false
+		}
+	});
+
+	var chartC = bb.generate({
+		bindto : "#pieChartC",
+		size : {
+			height : 150,
+			width : 120
+		},
+		data : {
+			columns : [ [ "data1", 30 ], [ "data2", 120 ] ],
+			type : "donut",
+		},
+		donut : {
+			title : "컨텐츠",
+			label : {
+				show : false,
+			},
+		},
+		legend : {
+			show : false
+		}
+	});
+
+	var timeChart = bb.generate({
+		bindto : "#timeChart",// +거래량 콤보
+		data : {
+			x : "x",
+			columns : [
+					[ "x", "2013-01-01", "2013-01-02", "2013-01-03",
+							"2013-01-04", "2013-01-05", "2013-01-06" ],
+					[ "data1", 30, 200, 100, 400, 150, 250 ],
+					[ "data2", 130, 340, 200, 500, 250, 350 ] ]
+		},
+		axis : {
+			x : {
+				type : "timeseries",
+				tick : {
+					format : "%Y-%m-%d"
+				}
+			}
+		},
+		legend : {
+			position : "right"
+		},
+		size : {
+			height : 210,
+			width : 500
+		},
+		resize : {
+			auto : true
+		},
+		padding : {
+			top : 10,
+			bottom : 10
+		}
+	});
+
+	setTimeout(function() {
+		chart.load({
+			columns : [ [ "data3", 400, 500, 450, 700, 600, 500 ] ]
+		});
+	}, 1000);
+
+	var chart = bb.generate({
+		bindto : "#side",
+		data : {
+			columns : [ [ "data", 91.4 ] ],
+			type : "gauge",
+		},
+		gauge : {},
+		color : {
+			pattern : [ "#FF0000", "#F97600", "#F6C600", "#60B044" ],
+			threshold : {
+				values : [ 30, 60, 90, 100 ]
+			}
+		},
+		size : {
+			height : 150,
+		// width : 150
+		},
+	});
 });
