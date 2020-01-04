3d089df96a676037b9d1c36de3c9480585c198b5
bluewow <dogseen@gmail.com>
2020-01-04 23:13:01 +0900

    종목에 따른 curStock 반영

--- mainProject/src/com/stockmarket/www/controller/trade/AnalysisController.java
+++ mainProject/src/com/stockmarket/www/controller/trade/AnalysisController.java
@@ -3,6 +3,7 @@ package com.stockmarket.www.controller.trade;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,6 +14,8 @@ import javax.servlet.http.HttpSession;
 
 import org.json.simple.JSONObject;
 
+import com.stockmarket.www.controller.system.AppContext;
+import com.stockmarket.www.entity.CurStock;
 import com.stockmarket.www.service.AnalysisService;
 import com.stockmarket.www.service.basic.BasicAnalysisService;
 
@@ -43,11 +46,31 @@ public class AnalysisController extends HttpServlet{
 		String codeNum = request.getParameter("codeNum");
 		if(codeNum != null) {
 			HashMap<Object, Object>map = new HashMap<>();
+			List<CurStock> stocks = AppContext.getStockMarket();
+			CurStock curStock = null; 
+			if(stocks == null) {
+				curStock = new CurStock(codeNum, "[데이터 수집중...]", "보합", "0", "none", "0");
+			} else {
+				for(CurStock stock : stocks) {
+					if(stock.getCodeNum().equals(codeNum)) {
+						curStock = new CurStock(
+								codeNum,
+								stock.getPrice(),
+								stock.getGain(), 
+								stock.getGainPrice(), 
+								stock.getSignMark(), 
+								stock.getPercent());
+						break;
+					}
+				}
+			}
+			
+//			System.out.println(curStock.toString());  //for debugging
 			map.put("name", service.getStockName(codeNum));
-			map.put("price", 20000);
-			map.put("status", "up"); //or down
-			map.put("unit", 40000);
-			map.put("ratio", "1.13");
+			map.put("price", curStock.getPrice());
+			map.put("status", curStock.getGain()); // 상승, 보합, 하락
+			map.put("gain", curStock.getGainPrice());
+			map.put("ratio", curStock.getPercent());
 			response.setContentType("application/json; charset=UTF-8");
 			PrintWriter out = response.getWriter();
 			out.print(new JSONObject(map));
