261758fc2a2c978d7f08da945841bab3df51aef8
bluewow <dogseen@gmail.com>
2020-01-11 00:02:59 +0900

    분석창 기본 look & feel 완료

--- mainProject/WebContent/card/trade/trading.jsp
+++ mainProject/WebContent/card/trade/trading.jsp
@@ -14,57 +14,58 @@
 <!-- download fontawesome.com -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
+<script src="https://d3js.org/d3.v4.min.js"></script>
+<!--다운 받아서 포함 시키기-->
+<link rel="stylesheet" href="../../css/billboard.css">
+<script src = "../../js/billboard.js"></script>
+
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script type="text/javascript" src="../../js/trade/trade.js"></script>
 	 
 	 
 </head>
-<body class="scrollbar custom-scrollbar-style">
+<body>
 <div id="trading-container">
 	<!-- --------------- page-top -------------- -->
 	<header class="page-top">
-		<div id="stockName">
-			<div></div>		<!-- 종목명 -->
-			<div></div>		<!-- 현재가 -->
-			<span></span> 	<!-- up/down 기호-->
-			<span></span>	<!-- 원 -->
-			<span></span>	<!-- % -->
-		</div>
+		<div id="title">호가창</div>
 	</header>
 	<!-- TODO 장이마감되었습니다 -->
 	<!-- --------------- page-mid -------------- -->
 	<section class="page-mid">
-<!-- 		
-		<form action="trade" method="get">
-			<input class="button button-chart day_" type="submit" name="date" value="일봉">
-			<input class="button button-chart" type="submit" name="date" value="주봉">
-			<input class="button button-chart" type="submit" name="date" value="월봉">
-		</form> 
--->
-		<div id="chart_div"></div>
+		<div id="chartSell"></div>
+		<div id="chartBuy"></div>
 	</section>
 	
 	<!-- --------------- page-bottom -------------- -->
 		<section class="page-bottom">
 		<form id="page-bottom-box" action="trade" method="get">
 			<div class="show-button-align">
-				<input class="button button-status" type="button" value="평균 단가">
-				<div class="data"><fmt:formatNumber value="${aveAssets}" pattern="#,###" />원</div>
-				<input class="button button-status" type="button" value="보유 수량">
-				<div class="data"><fmt:formatNumber value="${myQuantity }" pattern="#,###" />주</div>	
-				<input class="event button button-button animation" type="button" name="trade" value="매       수">
+				<input class="button button-status" type="button" value="자산상황">
+				<div class="data"><fmt:formatNumber value="${myAssets }" pattern="#,###" />원</div>
 
+				<input class="button button-status" type="button" value="   단가   ">
+				<div style="position: relative;">
+					<input type="text" value="" name="quantity" class="button text">
+					<i class="fa fa-caret-up fa-lg caret-up" aria-hidden="true"></i>
+					<i class="fa fa-caret-down fa-lg caret-down" aria-hidden="true"></i>
+				</div>
+				
+
+				<input class="event button button-button animation" type="button" name="trade" value="매       수">
 			</div>
 			<div class="show-button-align">
-				<input class="button button-status" type="button" value="가상 머니">
-				<div class="data"><fmt:formatNumber value="${myAssets }" pattern="#,###" />원</div>
-				<input class="button button-status" type="button" value="구매/매도">
-				<div>
-					<!-- enter 입력을 막음 -->
-					<input class="data" id="text" type="text" name="Purse/Sold" autocomplete="off" onkeydown="if(event.keyCode == 13) return false;"> 
+				<input class="button button-status" type="button" value="보유수량">
+				<div class="data"><fmt:formatNumber value="${myQuantity }" pattern="#,###" />주</div>	
+				
+				<input class="button button-status" type="button" value="   수량   ">
+				<div style="position: relative;">
+					<input class="data text" type="text" name="Purse/Sold" autocomplete="off" onkeydown="if(event.keyCode == 13) return false;">
+					<i class="fa fa-caret-up fa-lg caret-up" aria-hidden="true"></i>
+					<i class="fa fa-caret-down fa-lg caret-down" aria-hidden="true"></i>
 				</div>
+				
 				<input id="sell" class="event button button-button animation" type="button" name="trade" value="매       도">
-				<%-- <input class="event button button-button animation ${shadow }" type="submit" ${disable } name="trade" value="매       도"> --%>
 			</div>
 		</form>
 	</section>
--- mainProject/WebContent/js/trade/trade.js
+++ mainProject/WebContent/js/trade/trade.js
@@ -3,20 +3,88 @@ var codeNum = "005930"; //삼성전자
 window.addEventListener("message", function(e) {
 	if(e.data && (e.data.length == 6)) { //codeNum
 		codeNum = e.data;
-		updateEvent();
 		
 		asyncTrade("pass", 0, e.data);		// 매수/매도 창 데이터 갱신
 	}
 });
 
 window.addEventListener("load", function(){
-	updateEvent();
-	
+	bb.defaults();
+	chartUpdate();
 	asyncTrade("pass", 0);		
 	tradeFunc();		// 매수/매도 이벤트 처리
 
 
 });
+
+function chartUpdate() {
+	chartSell();
+	chartBuy();
+}
+
+bb.defaults({
+	data: {
+		type: "bar",
+		labels:{
+			colors : "black",
+			centered : true,
+		  	position:{ x:10 },
+		},
+	},
+	axis: {
+        rotated: true,
+        y: {
+            show: false,
+            max: 6000,
+        },
+        x: { type: "category" }
+    },
+    legend: { show: false },
+    tooltip: { show: false },
+    bar: { padding: 5 },
+    size: {
+    	width: 200,
+        height: 350
+    },
+//    onresize: function(ctx) {
+//    	//TODO
+//    }
+	
+});
+
+function chartSell() {
+	var chart = bb.generate({
+		bindto : "#chartSell",
+	    data: {
+	    	x: "x",
+	        columns: [	   
+	        	["x",2200,2300,2400,2500,2550,2600,2650,2700,2750,2800],
+	        	["data", 100,8000,700,16,51,21,45,66,237,1300],
+	        ],
+	        colors:{data:"#2D9AF277"}
+	    },
+	    title: {
+	        text: "매도잔량"
+	    }
+	});
+}
+
+function chartBuy() {
+	var chart = bb.generate({
+		bindto : "#chartBuy",
+	    data: {
+	    	x: "x",
+	        columns: [	            
+	        	["x",1650,1700,1750,1800,1850,1900,1950,2000,2050,2100],
+	        	["data", 1,0,2,4,5,6,7,8,9,2500],
+	        ],	
+	        colors:{data:"#F4000677"}
+	    },
+	    title: {
+	        text: "매수잔량"
+	    }
+	});
+}
 
 function asyncGraph() { 
     var ajax = new XMLHttpRequest();
@@ -26,36 +94,6 @@ function asyncGraph() {
     			drawBasic(JSON.parse(ajax.responseText)));
     }
     ajax.send();
-}
-
-function updateEvent() {
-	var stockNameDiv = document.querySelectorAll("#stockName div");
-	var stockNameSpan = document.querySelectorAll("#stockName span");
-
-	var ajax = new XMLHttpRequest();
-	ajax.open("GET", "../../card/trade/trade?codeNum=" + codeNum);
-	ajax.onload = function() {
-		var obj = JSON.parse(ajax.responseText);
-		stockNameDiv[0].innerHTML = obj.name;
-		stockNameDiv[1].innerHTML = obj.price.toLocaleString();
-		if(obj.status == "up") {
-			stockNameSpan[0].classList.add("fa", "fa-caret-up");
-			stockNameSpan[1].innerHTML = obj.unit.toLocaleString();
-			stockNameSpan[2].innerHTML = "(+"+ obj.ratio + "%)";
-			for(var i = 0; i < stockNameSpan.length; i++) {
-				stockNameSpan[i].style.color = "red";
-			}
-		}
-		if(obj.status == "down") {
-			stockNameSpan[0].classList.add("fa", "fa-caret-down");
-			stockNameSpan[1].innerHTML = obj.unit.toLocaleString();
-			stockNameSpan[2].innerHTML = "(-"+ obj.ratio + "%)";
-			for(var i = 0; i < stockNameSpan.length; i++) {
-				stockNameSpan[i].style.color = "blue";
-			}
-		}
-	}
-	ajax.send();
 }
 
 function asyncTrade(p1, p2) { //p1=buy/sell p2=수량 p3=종목코드
@@ -68,10 +106,10 @@ function asyncTrade(p1, p2) { //p1=buy/sell p2=수량 p3=종목코드
     		+ p1 + "&Purse/Sold=" + p2 + "&codeNum" + codeNum );
     ajax.onload = function() {
     	var obj = JSON.parse(ajax.responseText);
-        data[0].innerHTML = obj.avgPrice.toLocaleString() + "원";
+        data[0].innerHTML = obj.vMoney.toLocaleString() + "원";
         data[1].innerHTML = obj.quantity.toLocaleString() + "주";
-        data[2].innerHTML = obj.vMoney.toLocaleString() + "원";
-        data[3].value = "";
+//        data[2].innerHTML = obj.vMoney.toLocaleString() + "원";
+//        data[3].value = "";
 
 		if(data[1].innerHTML == "0주") {
 			sellButton.className = "event button button-button shadow"
@@ -112,44 +150,79 @@ function asyncTrade(p1, p2) { //p1=buy/sell p2=수량 p3=종목코드
 
 //  매수/매도 이벤트 처리
 function tradeFunc() {
-	var button = document.querySelector("#page-bottom-box");
-	var event = button.querySelector("input.event");
-	var data = button.querySelectorAll(".data");
-	var qty = button.querySelector("#text");
-	var sellButton = button.querySelector("#sell");
-	
-	button.onclick = function(e) {
-		if(e.target.name != event.name)
-	      	return;
-	    
-		if(!data[3].value) {
-			alert("수량을 입력해 주세요");
-			return;
-		}
-		
-		if(data[3].value <= 0) {
-			alert("잘못된 수량입력 입니다");
-			data[3].value = "";
-			return;
-		}
-		
-		if(e.target.value == "매       수") {
-			var trade = "buy";
-			if(confirm(data[3].value + "주 매수를 진행하시겠습니까?") == false) {
-				data[3].value = "";
-				return;
-			}
-			
-		}
-		else if(e.target.value =="매       도") {
-			var trade = "sell";
-			if(confirm(data[3].value + "주 매도를 진행하시겠습니까?") == false) {
-				data[3].value = "";
-				return;
-			}
-		}
-			
-		asyncTrade(trade, qty.value);
-	}
+//	var button = document.querySelector("#page-bottom-box");
+//	var event = button.querySelector("input.event");
+//	var data = button.querySelectorAll(".data");
+//	var qty = button.querySelector("#text");
+//	var sellButton = button.querySelector("#sell");
+//	
+//	button.onclick = function(e) {
+//		if(e.target.name != event.name)
+//	      	return;
+//	    
+//		if(!data[3].value) {
+//			alert("수량을 입력해 주세요");
+//			return;
+//		}
+//		
+//		if(data[3].value <= 0) {
+//			alert("잘못된 수량입력 입니다");
+//			data[3].value = "";
+//			return;
+//		}
+//		
+//		if(e.target.value == "매       수") {
+//			var trade = "buy";
+//			if(confirm(data[3].value + "주 매수를 진행하시겠습니까?") == false) {
+//				data[3].value = "";
+//				return;
+//			}
+//			
+//		}
+//		else if(e.target.value =="매       도") {
+//			var trade = "sell";
+//			if(confirm(data[3].value + "주 매도를 진행하시겠습니까?") == false) {
+//				data[3].value = "";
+//				return;
+//			}
+//		}
+//			
+//		asyncTrade(trade, qty.value);
+//	}
 }
 
+//function incrementValue(e) {
+//	e.preventDefault();
+//	var fieldName = $(e.target).data('field');
+//	var parent = $(e.target).closest('div');
+//	var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+//	
+//	if (!isNaN(currentVal)) {
+//	  parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
+//	} else {
+//	  parent.find('input[name=' + fieldName + ']').val(0);
+//	}
+//}
+//
+//function decrementValue(e) {
+//	e.preventDefault();
+//	var fieldName = $(e.target).data('field');
+//	var parent = $(e.target).closest('div');
+//	var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+//	
+//	if (!isNaN(currentVal) && currentVal > 0) {
+//	  parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
+//	} else {
+//	  parent.find('input[name=' + fieldName + ']').val(0);
+//	}
+//}
+//
+//$('.input-group').on('click', '.button-plus', function(e) {
+//	  incrementValue(e);
+//	});
+//
+//	$('.input-group').on('click', '.button-minus', function(e) {
+//	  decrementValue(e);
+//	});
+
+	
